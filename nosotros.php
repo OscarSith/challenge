02a08ae7471c0d91693be61faf5cae7275d163cb
Oscar Larriega - Personal Macbook Pro <larriega@gmail.com>
2015-03-13 00:50:37 -0500

    Paginas echas, flyers terminados, responsitive terminado en mas de 75%

---
+++ nosotros.php
@@ -0,0 +1,8 @@
+<?php include 'tpl/header.html' ?>
+	<article>
+		<h4>MISIÓN</h4>
+		<p>Challenger eventos busca satisfacer las necesidades de sus clientes brindando un excelente servicio de fuegos artificiales en los que predomina el profesionalismo, seguridad e innovación para el disfrute de lo último en el arte de iluminar el cielo.</p>
+		<h4>VISIÓN</h4>
+		<p>Challenger eventos desea convertirse en la empresa más reconocida en el rubro de fuegos artificiales en el Perú, gracias a su excelente servicio en todos los espectáculos activados por profesionales y la autorización de entidades que garantizan la seguridad de nuestros productos pirotécnicos.</p>
+	</article>
+<?php include 'tpl/footer.html' ?>
